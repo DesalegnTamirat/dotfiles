7f8c3ff1a25ff4b92f3aefb1c9737abe99846bb4
Desalegn Tamirat <81221215+DesalegnTamirat@users.noreply.github.com>
2024-12-12 17:07:59 +0300

    Update settings.js

--- .vscode/settings.js
+++ .vscode/settings.js
@@ -1,1 +1,10 @@
 
+{
+    "workbench.iconTheme": "material-icon-theme",
+    "workbench.colorTheme": "Dark Magic",
+    "editor.fontWeight": "bold",
+    "editor.fontSize": 20,
+    "editor.lineHeight": 1.9,
+    "terminal.integrated.fontSize": 16,
+    "terminal.integrated.fontWeight": "normal",
+}
